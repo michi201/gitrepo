4630b423fe61f7cad1c9047d38bff007fb368b85
michi201 <mp.2000@wp.pl>
2017-09-25 12:36:03 +0200

    dodanie kodu obliczenie boku kwadratu

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,34 +1,29 @@
 /*
  * hello.cpp
- * 
- * Copyright 2017  <>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
  */
 
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	cout << "Witaj w C++";
+    // char imie; // deklaracja zmiennej znakowej
+    char imie[10]; // deklaracja zmiennej tablicowej
+    int wiek;
+    wiek = 0; //inicjacja zmiennej
+    
+	cout << "Witaj w C++" << endl;
+	cout << "Podaj imię: ";
+    //cin >> imie;
+    cin.getline(imie, 10);
+    cout << "Cześć, " << imie << "!" << endl;
+    cin.ignore(100, '/n');
+    cout << "Ile masz lat? ";
+    cin >> wiek;
+    cout << "Urodziłeś się w roku " << 2017 - wiek << endl;
 	return 0;
 }
 
