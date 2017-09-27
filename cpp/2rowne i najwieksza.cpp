52deee869bbe5d4cc27943decfa0eabcc47e17d2
michi201 <mp.2000@wp.pl>
2017-09-27 22:17:55 +0200

    Ulepszenie kodu z 2 równymi i najwiekszą

--- cpp/2rowne i najwieksza.cpp
+++ cpp/2rowne i najwieksza.cpp
@@ -12,36 +12,70 @@ int main(int argc, char **argv)
     int a, b, c;
     a = b = c = 0;
     
+    for(;;)
+    
+{    
+
     cout << "podaj bok a: " <<endl;
     cin >> a;
     cout << "podaj bok b: " <<endl;
     cin >> b;
     cout << "podaj bok c: " <<endl;
     cin >> c;
-    
+  
 
-
-
-{   if ((c > b)&&(c > a))
-        cout << "Najwieksze jest c" << endl;
+    if (!cin) // jeżeli wartość jest literą
+        {
+         system( "cls" );
+         cin.clear(); //czyści flagi błędów np. wyczyścić a="e" żeby mogło byc a=0                                
+         cin.sync(); //czyści bufor
+         cout << "Podales bledna wartosc, sprobuj ponownie" << endl;
+        }
     else
-        if ((b > c)&&(b > a))
+    if ((c > b) && (c > a))
+    {
+        cout << "Najwieksze jest c" << endl;
+        exit(0);
+    }
+    else
+    if ((b > c) && (b > a))
+    {
         cout << "Najwieksze jest b" << endl;
-         else
-            if ((a > b)&&(a > c))
-                cout << "Najwieksze jest a" << endl;
-            else
-                 if ((a > c)&&(a = b))
-                    cout << "Najwieksze jest a i b" << endl;
-                else
-                    if ((b > a)&&(b = c))
-                        cout << "Najwieksza jest b i c" << endl;
-                    else
-                        if ((a > b)&&(a = c))
-                            cout << "Najwieksze jest a i c" << endl;
-                                else
-                                    if ((a = b)&&(a = c)&&(b = c))
-                                        cout << "Wszystkie sa rowne" << endl;
+    
+    }
+    else
+    if ((a > b) && (a > c))
+    {
+        cout << "Najwieksze jest a" << endl;
+        exit(0);
+    }
+    else
+    if ((a > c) && (a = b))
+    {
+        cout << "Najwieksze jest a i b" << endl;
+        exit(0);
+    }
+    else
+    if ((b > a) && (b = c))
+    {
+        cout << "Najwieksza jest b i c" << endl;
+        exit(0);
+    }
+    else
+    if ((a > b) && (a = c))
+    {
+        cout << "Najwieksze jest a i c" << endl;
+        exit(0);
+    }
+    else
+    if ((a = b) && (a = c) && (b = c))
+    {
+        cout << "Wszystkie sa rowne" << endl;
+        exit(0);
+    }
+    else
+        ;
+
 }
 	return 0;
 }
