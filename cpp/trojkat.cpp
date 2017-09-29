73569c40f66cac0bad3fda21ee48d7094fb570d2
michi201 <mp.2000@wp.pl>
2017-09-29 08:45:59 +0200

    Dodanie obliczenia pola trójkąta

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -4,17 +4,31 @@
 
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
     int a, b, c;
+    int obwod = 0;
+    float p = 0;
     a = b = c = 0;
     
 	cout << "Podaj boki:" << endl;
     cin >> a >> b >> c;
-    cout << "Obwod: " << a + b + c  << endl;
-	return 0;
+    if (( a + b > c) && (a + c > b) && (b + c > a))
+    {
+        obwod = a + b + c;
+        cout << "Trójkąt istnieje" << endl;
+        cout << "Obwod: " << obwod << endl;
+        p = 0.5 * obwod;
+        cout << " Współczynnik: " << p << endl;
+        cout << "Pole: " << sqrt(p * (p-a) * (p-b) * (p-c)) << endl;
+    }
+    else {
+        cout << "Trójkąt nie istnieje" << endl;    
+	}
+    return 0;
 }
 
