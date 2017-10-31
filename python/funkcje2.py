07bf35dc2c640f9e1525ebcd0102f28faa687c82
michi201 <mp.2000@wp.pl>
2017-10-31 10:36:19 +0100

    funkcje py

---
+++ python/funkcje2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  funkcje2.py
+
+# w funkcjach zmienne nie wiążące się z innymi zmiennymi w innych funkcjach
+def sumuj(x, y):
+    return x + y
+    
+def odejmij(c, d):
+    return c - d
+
+def main(args):
+    a = int(input("Podaj liczbe: "))
+    b = int(input("Podaj liczbe: "))
+    
+    
+    print("Suma: ", sumuj(a,b))
+    print("Różnica: ", odejmij(a,b))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
