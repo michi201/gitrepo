cc0bcb162a37013d5469d052ab56cbb4a38564bb
michi201 <mp.2000@wp.pl>
2017-12-11 12:32:34 +0100

    pierwsze rekurencje

--- python/ciag_fibonacciego.py
+++ python/ciag_fibonacciego.py
@@ -21,6 +21,14 @@ def fib_iter(n):
     return b
 
 
+# fib_rek(n) = 1 dla n {0, 1}
+# fib_rek(n) = fib_rek(n-1) + fib_rek(n-2) dla n > 1
+def fib_rek(n):
+    if n < 2:
+        return 1
+    return fib_rek(n - 1) + fib_rek(n - 2)
+
+
 def main(args):
     n = int(input("Numer wyrazu w ciągu: "))
     # assert fib_iter(0) == 0
--- python/potega.py
+++ python/potega.py
@@ -2,29 +2,38 @@
 # -*- coding: utf-8 -*-
 #
 #  potega.py
-  
+
+
 def potega_it(x, n):
-	
     i = 1
     wynik = 1
     while i <= n:
         wynik = wynik * x
         i = i + 1
-        
     return wynik
+
+
+# potega_rek(x, 0) = 1 dla x różnego od 0
+# potega_rek(x, n) = potega_rek(x, n-1) * x dla n = N+
+def potega_rek(x, n):
+    if x == 0:
+        return 0
+    elif n == 0:
+        return 1
+    return potega_rek(x, n - 1) * x
 
 
 def main(args):
     x = float(input("Podaj podstawe potęgi: "))
     n = int(input("Podaj wykładnik potęgi: "))
-    assert type(n) == int 
+    assert type(n) == int
     assert potega_it(1, 1) == 1
     assert potega_it(2, 1) == 2
     assert potega_it(2, 3) == 8
-    assert potega_it(5, 2) == 25 
-    
-    print('Wynik: ', potega_it(x, n))
+    assert potega_it(5, 2) == 25
+    print('Wynik: ', potega_rek(x, n))
     return 0
+
 
 if __name__ == '__main__':
     import sys
