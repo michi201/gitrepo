018c6598a2e4d3d0becb7e7f20389d8bf3405b73
michi201 <mp.2000@wp.pl>
2018-12-03 09:40:27 +0100

    kwerendy orm

--- Bazy danych/uczniowie/kwerendy_orm.py
+++ Bazy danych/uczniowie/kwerendy_orm.py
@@ -41,12 +41,66 @@ def kw03():
         print(obj.uczen.nazwisko, obj.ile)
 
 
+def kw04():
+    # po ile uczniów maja klasy
+    query = (Uczen
+             .select(Uczen.klasa.klasa, fn.Count(Uczen.nazwisko).alias("ile"))
+             .join(Klasa)
+             .group_by(Uczen.klasa.klasa)
+             .order_by(SQL("ile").asc())
+             )
+    for obj in query:
+        print(obj.klasa.klasa, obj.ile)
+
+
+def kw05():
+    # śrenia ocen z poszczególnych przedmiotów
+    query = (Ocena
+             .select(Ocena.przedmiot.przedmiot,
+                     fn.AVG(Ocena.ocena).alias("srednia"))
+             .join(Przedmiot)
+             .group_by(Ocena.przedmiot.przedmiot)
+             .order_by(SQL("srednia").desc())
+             )
+    for obj in query:
+        print(obj.przedmiot.przedmiot, obj.srednia)
+
+
+def kw06():
+    # śrenia ocen poszczególnych uczniów
+    query = (Ocena
+             .select(Ocena.uczen.nazwisko,
+                     fn.AVG(Ocena.ocena).alias("srednia"))
+             .join(Uczen)
+             .group_by(Ocena.uczen.nazwisko)
+             .order_by(SQL("srednia").asc())
+             )
+    for obj in query:
+        print(obj.uczen.nazwisko, obj.srednia)
+
+
+def kw07():
+    # oceny ucznia Szymczak z poszczególnych przedmiotów
+    query = (Ocena
+             .select(Ocena.uczen.nazwisko, Ocena.przedmiot.przedmiot,
+                     Ocena.ocena)
+             .join(Uczen, Przedmiot)
+             .where(Ocena.uczen.nazwisko == "Szymczak")
+             )
+    for obj in query:
+        print(obj.uczen.nazwisko, obj.przedmiot.przedmiot, obj.ocena)
+
+
 def main(args):
     baza.connect()
 
     # kw01()
     # kw02()
-    kw03()
+    # kw03()
+    # kw04()
+    # kw05()
+    # kw06()
+    kw07()
 
     baza.close()
     return 0
